e9dcdfd42a70d3e85f1851341825e2af37fff0e8
ryouji0958 <ryouji0958@163.com>
2019-10-04 17:51:24 +0800

    第二次提交

--- src/com/chinalife/HelloWorld.java
+++ src/com/chinalife/HelloWorld.java
@@ -1,13 +1,9 @@
 package com.chinalife;
 
-/**
- * @description: my first commit git
- * @author: zhangqunling
- * @create: 2019-10-04 17:21
- * @version: 1.0
- **/
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("2222222222222第二次提交");
     }
 }
