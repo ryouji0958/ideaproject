3de8a041d3eddfdb1b2d96fc15caea33e52035d6
ryouji0958 <51151374+ryouji0958@users.noreply.github.com>
2019-10-04 17:52:35 +0800

    Update HelloWorld.java

--- src/com/chinalife/HelloWorld.java
+++ src/com/chinalife/HelloWorld.java
@@ -4,6 +4,6 @@ package com.chinalife;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
-        System.out.println("2222222222222第二次提交");
+        System.out.println("在远程仓库修改的记录");
     }
 }
