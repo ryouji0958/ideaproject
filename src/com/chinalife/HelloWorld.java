37c950ff3792f73a700f3efea0e642828d40470d
ryouji0958 <ryouji0958@163.com>
2019-10-04 17:28:31 +0800

    First commit all

---
+++ src/com/chinalife/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.chinalife;
+
+/**
+ * @description: my first commit git
+ * @author: zhangqunling
+ * @create: 2019-10-04 17:21
+ * @version: 1.0
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
